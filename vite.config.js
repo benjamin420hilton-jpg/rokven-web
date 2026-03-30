f363a3d1da504ca536cb301c4e4ddbba458a558d
Benjamin Hilton <benjamin420hilton@example.com>
2026-03-30 11:55:13 +0800

    fix: clean up vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'; // Or whatever framework plugin you are using
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/rokven-web/', // <--- ADD THIS LINE
-  plugins: [react()], // Your existing plugins
-  // ... other configurations
+  base: '/rokven-web/',
+  plugins: [react()],
 });
